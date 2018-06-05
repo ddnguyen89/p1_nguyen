fb6b7348ed77ea97f30dd585f028ffe38098f2a2
eatdabanunu <duynguyen.xz@gmail.com>
2018-06-05 01:01:17 -0700

    6/5/2018

---
+++ src/pages/buyout/buyout.ts
@@ -0,0 +1,34 @@
+  import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { HomePage } from '../home/home';
+
+/**
+ * Generated class for the BuyoutPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-buyout',
+  templateUrl: 'buyout.html',
+})
+export class BuyoutPage {
+
+  itemName: string;
+  itemQuantity: string;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.itemName = navParams.get('dataName');
+    this.itemQuantity = navParams.get('dataQuantity');
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad BuyoutPage');
+  }
+
+  goToHomePage() {
+    this.navCtrl.push(HomePage)
+  }
+}
---
+++ src/pages/shop/shop.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { BuyoutPage } from '../buyout/buyout';
+
+/**
+ * Generated class for the ShopPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-shop',
+  templateUrl: 'shop.html',
+})
+export class ShopPage {
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ShopPage');
+  }
+
+  goToBuyoutPage(itemName, itemQuantity) {
+    this.navCtrl.push(BuyoutPage, {
+      dataName: itemName,
+      dataQuantity: itemQuantity
+    })
+  }
+
+}
